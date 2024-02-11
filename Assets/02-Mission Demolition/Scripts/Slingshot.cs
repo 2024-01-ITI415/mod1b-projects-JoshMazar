e0e83e4c1ea7bc7561de5fccb8550f92a917834a
JoshMazar <157077309+JoshMazar@users.noreply.github.com>
2024-02-11 00:22:26 -0500

    Got up to hitting the goal
    going to bed

--- Assets/02-Mission Demolition/Scripts/Slingshot.cs
+++ Assets/02-Mission Demolition/Scripts/Slingshot.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    static private Slingshot S; // a
     // fields set in the Unity Inspector pane
     [Header("Set in Inspector")] // a
     public GameObject prefabProjectile;
@@ -17,8 +18,18 @@ public class Slingshot : MonoBehaviour
     public GameObject projectile; // b
     public bool aimingMode; // b
     private Rigidbody projectileRigidbody; // a
+    static public Vector3 LAUNCH_POS
+    { // b
+        get
+        {
+            if (S == null) return Vector3.zero;
+            return S.launchPos;
+        }
+    }
+
     void Awake()
     {
+        S = this; // c
         Transform launchPointTrans = transform.Find("LaunchPoint"); // a
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive(false); // b
